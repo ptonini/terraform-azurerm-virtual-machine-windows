3da1bf807cd7212d75462731a103ae52d5e43f41
ptonini <pedro.tonini@hotmail.com>
2023-12-12 12:57:07 -0300

    fix: fixed outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "this" {
-  value = azurerm_linux_virtual_machine.this
+  value = azurerm_windows_virtual_machine.this
 }
 
-output "network_interfaces" {
-  value = [for ni in module.network_interface : ni.this]
+output "network_interface_ids" {
+  value = module.requirements.network_interface_ids
 }
 
 output "public_ips" {
-  value = [for ni in module.network_interface : ni["public_ip"]]
+  value = module.requirements.public_ips
 }
